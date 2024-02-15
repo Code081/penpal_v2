7b57e1c628fe4a510dcdbe223d097ab9675c989a
Code081 <shashwatp011@gmail.com>
2024-02-15 23:42:25 +0530

    Add character bounding boxes to image

--- pythonOCR.py
+++ pythonOCR.py
@@ -5,13 +5,27 @@ from gtts import gTTS
 # from playsound import playsound
 
 image_1 = cv2.imread('./Tests/Image 1.png')
+
+height, width, _ = image_1.shape
+
+
 # image_2 = cv2.imread('image_2.png')
 # image_3 = cv2.imread('image_3.png')
 
-# cv2.imshow('Image 1', image_1)
+cv2.imshow('Image 1', image_1)
 # cv2.imshow('Image 2', image_2)
 # cv2.imshow('Image 3', image_3)
 
 print(pytesseract.image_to_string(image_1))
+
+character_box = pytesseract.image_to_boxes(image_1)
+
+for a in character_box.splitlines():
+    a = a.split(' ')
+    print(a)
+    x, y, w, h = int(a[1]), int(a[2]), int(a[3]), int(a[4])
+    cv2.rectangle(image_1, (x, height-y), (w, height-h), (0, 0, 255), 1)
+    
+cv2.imshow('Image 1', image_1)
 
 cv2.waitKey(0)
