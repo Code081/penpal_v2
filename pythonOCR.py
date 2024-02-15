9009df99554316d5b48f38a5f3d9e13ea140ee22
Shashwat <70833832+Code081@users.noreply.github.com>
2024-02-15 17:43:00 +0000

    Add OCR functionality to pythonOCR.py

--- pythonOCR.py
+++ pythonOCR.py
@@ -8,8 +8,10 @@ image_1 = cv2.imread('./Tests/Image 1.png')
 # image_2 = cv2.imread('image_2.png')
 # image_3 = cv2.imread('image_3.png')
 
-cv2.imshow('Image 1', image_1)
+# cv2.imshow('Image 1', image_1)
 # cv2.imshow('Image 2', image_2)
 # cv2.imshow('Image 3', image_3)
+
+print(pytesseract.image_to_string(image_1))
 
 cv2.waitKey(0)
